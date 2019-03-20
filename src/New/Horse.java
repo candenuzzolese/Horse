67c42bca3cbf23df83ffe82c340c9f714eb06b9c
candenuzzolese <candela.nuzzolese@ing.austral.edu.ar>
2019-03-20 20:09:18 -0300

    horse position

---
+++ src/New/Horse.java
@@ -0,0 +1,21 @@
+package New;
+
+import java.util.ArrayList;
+
+public class Horse {
+
+    private Position currentPosition;
+    private ArrayList<Position> possiblePosition = new ArrayList<> (  );
+
+    public Horse(Position currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
+    void availableMovements(){
+
+
+        int1
+
+
+    }
+}
---
+++ src/New/Position.java
@@ -0,0 +1,25 @@
+package New;
+
+public class Position{
+        public static char[] letters = {'A','B','C','D','E','F','G','H'};
+
+        private int number;
+        private int letter;
+
+        public Position(int number, int letter) {
+            this.number = number;
+            this.letter = letter;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public int getLetter() {
+            return letter;
+        }
+
+        public void print() {
+            System.out.print(letters[letter] + (number + 1));
+        }
+}
