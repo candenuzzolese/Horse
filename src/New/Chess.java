e3f202eda82f5b9d3d3682f325dea11f93c9742e
candenuzzolese <candela.nuzzolese@ing.austral.edu.ar>
2019-03-20 22:27:38 -0300

    stack

---
+++ src/New/Chess.java
@@ -0,0 +1,34 @@
+package New;
+
+import java.util.Stack;
+
+public class Chess {
+
+
+        Horse horse;
+        Position board [][];
+        Stack <Position> movements = new Stack ();
+
+
+
+        public Chess (int n, Horse horse) {
+            this.horse = horse;
+
+            board = new Position[8][8];
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    board[i][j] = new Position(i, j);
+                }
+            }
+            horse.setCurrentPosition (board[0][0]);
+        }
+
+        public void start(){
+
+
+        }
+
+
+
+    }
+
---
+++ src/New/Stack.java
@@ -0,0 +1,14 @@
+package New;
+
+public interface Stack<T>{
+
+    void push(T t);
+    void pop();
+    T peek();
+    boolean isEmpty();
+    int size();
+    void empty();
+}
+
+
+
